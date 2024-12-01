758496c1f0ee34bf5561aca7b14657a1bd7e8ed5
osel13 <Honzzaher@seznam.cz>
2024-12-01 12:43:29 +0100

    scenes done

---
+++ intro.gd
@@ -0,0 +1,15 @@
+extends Control
+var intro = preload("res://background.tscn") #insert name of main scene here
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_timer_timeout() -> void:
+	get_tree().change_scene_to_file(game)
---
+++ main_menu.gd
@@ -0,0 +1,18 @@
+extends Control
+@export var audio : AudioStreamPlayer2D
+
+var intro = preload("res://intro.tscn")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_play_button_down() -> void:
+	get_tree().change_scene_to_file(intro)
+
+func _on_quit_button_down() -> void:
+	get_tree().quit()
